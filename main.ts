fa3af40e2291212b3c52b771b3d8ea20a8da93ba
Udomomo <udomomo@gmail.com>
2023-10-25 21:43:41 +0900

    mark to solving puzzle

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 const prop = PropertiesService.getScriptProperties().getProperties()
+const NOW_SOLVING = "挑戦中"
+const SOLVED = "o"
 
 function doGet(e: GoogleAppsScript.Events.DoGet) {
 }
@@ -22,13 +24,19 @@ function randomPuzzle(): string {
   if (!sheet) {
     throw new Error("sheet not found");
   }
-  
+
+  // 挑戦中マーカーを削除する
+  sheet.getRange("C1:C10000").clearContent();
+
   const rows = sheet.getRange("A1:B10000").getValues();
-  const unsolvedRows = rows.filter(row => row[1] != "o").map(row => row[0])
+  const unsolvedRows = rows.filter(row => row[1] != SOLVED).map(row => row[0])
   if (!unsolvedRows) {
     return "問題はありません"
   }
   const nextPuzzle = unsolvedRows[Math.floor(Math.random() * unsolvedRows.length)]
+
+  // 出題する行の3列目にマーカーを記録する
+  sheet.getRange(nextPuzzle, 3).setValue(NOW_SOLVING)
 
   return `${nextPuzzle}問目\nhttps://wandsbox125.web.app/puzzle-tools/hex-editor/10000.html?id=${nextPuzzle}`
 }
